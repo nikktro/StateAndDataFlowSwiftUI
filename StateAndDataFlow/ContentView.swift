21cab1d1e76ae7a72d182ac411b79319522549f1
Nikolay Trofimov <nikktro@gmail.com>
2022-07-13 21:59:23 +0300

    tap me App

--- StateAndDataFlow/ContentView.swift
+++ StateAndDataFlow/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(tapCount)")
+                .font(.largeTitle)
+                .padding(.top, 100)
+            Spacer()
+            ButtonView(tapCount: $tapCount)
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ButtonView: View {
+    @Binding var tapCount: Int
+    
+    var body: some View {
+        Button(action: { tapCount += 1 }) {
+            Text("Tap Me")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+        }
+        .frame(width: 200, height: 60)
+        .background(Color.red)
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(Color.black, lineWidth: 4)
+        )
     }
 }
